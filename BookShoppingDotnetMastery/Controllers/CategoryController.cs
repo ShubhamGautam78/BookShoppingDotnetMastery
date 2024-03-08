61ec3114227db75c4b89583455465d6c85325196
Shubham Gautam <ShubhamGautam67@gmail.com>
2024-03-08 17:20:07 +0530

    Category list and theme added

---
+++ BookShoppingDotnetMastery/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookShoppingDotnetMastery.Data;
+using BookShoppingDotnetMastery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShoppingDotnetMastery.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+           IEnumerable<Category> categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
---
+++ BookShoppingDotnetMastery/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookShoppingDotnetMastery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShoppingDotnetMastery.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
